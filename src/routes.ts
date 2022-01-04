0084390b6ccedc9ce3de35bb2be4973a061fc07f
philipef <philipesferreiraa@gmail.com>
2022-01-04 14:05:28 -0300

    criando services e controller para update category

--- src/routes.ts
+++ src/routes.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 import { CreateCategoryController } from "./controllers/CreateCategoryController";
 import { DeleteCategoryController } from "./controllers/DeleteCategoryController";
 import { GetAllCategoriesController } from "./controllers/GetAllCategoriesController";
+import { UpdateCategotyController } from "./controllers/UpdateCategoryController";
 
 const router = Router();
 
 router.post("/categories", new CreateCategoryController().handle);
 router.get("/categories", new GetAllCategoriesController().handle);
 router.delete("/categories/:id", new DeleteCategoryController().handle);
+router.put("/categories/:id", new UpdateCategotyController().handle);
 
 export { router };
